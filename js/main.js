7830cdba1a4c9890f5b210f3c57b2d6614ce875a
Marko Tomasevic <marko83nbg@gmail.com>
2019-04-29 21:41:28 +0200

    initial commit

---
+++ js/main.js
@@ -0,0 +1,151 @@
+document.getElementById('start').addEventListener('click', () => location.assign('maumau.html'));
+
+
+var players = [];
+
+function addPlayer() {
+    var playersName = document.getElementById('name').value;
+    if ( playersName.length == 0 ) {
+        alert( 'Unesite ime igrača!' );
+        return;
+    }
+    if (players.find(player => player.name.toLowerCase() === playersName)) {
+        alert( `Igrac sa imenom ${playersName} vec postoji!`);
+        return;
+    }
+    players.push({
+        name: playersName,
+        score: 0,
+    });
+
+    var tableHtml = '<table class="table table-striped"><tr><th>Ime</th><th>Rezultat</th><th></th><th></th></tr>';
+
+    for ( var i = 0; i < players.length; i++) {
+        var idName = players[i].name.replace(/ /g, '').toLowerCase();
+        tableHtml += '<tr><td>' + players[i].name + '</td>'
+                       + '<td id = "final_score_'+idName+'" class = "player-score">' + players[i].score + '</td>'
+                       + '<td><input type = "text" id = "curent_score_'+idName+'"></td>'
+                       + '<td><button id = "btn_'+idName+'"  class = "btn btn-submitt" onclick = "addScore(this)">Dodaj</button></td>';
+    }
+    tableHtml += '</table>';
+    document.getElementById('players').innerHTML = tableHtml;
+    document.getElementById('name').value = '';
+      
+};
+
+
+function addScore(btn) {
+//     for( var i = 0; i < players.length; i++) {
+//         var score = document.getElementById('curent_score_'+players[i].name.replace(/ /g, '').toLowerCase()).value;
+//         // alert(score);
+//         var getScore = parseInt(document.getElementById('curent_score_'+players[i].name.replace(/ /g, '').toLowerCase()).value);
+//         players[i].score += getScore;
+//         document.getElementById('final_score_'+players[i].name.replace(/ /g, '').toLowerCase()).innerText = players[i].score;
+//         document.getElementById('curent_score_'+players[i].name.replace(/ /g, '').toLowerCase()).value = '';
+
+    //}
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    var id = btn.id.split('btn_')[1];
+    var scoreElement = document.getElementById('curent_score_' + id);
+    if (scoreElement.value === '') {
+        alert('Unesite rezultat!');
+        return;
+    }
+    var score = parseInt(scoreElement.value);
+    
+    var playerForUpdate = players.find(player => player.name.toLowerCase() === id);
+    playerForUpdate.score += score;
+
+    if (playerForUpdate.score < 0) {
+        document.getElementById('final_score_'+id).style.color = '#33cc33';
+    } else if (playerForUpdate.score >= 0 && playerForUpdate.score <= 250){
+        document.getElementById('final_score_'+id).style.color = '#000';
+    } else if (playerForUpdate.score > 250 && playerForUpdate.score <= 380) {
+        document.getElementById('final_score_'+id).style.color = '#ffcc00';
+    } else if (playerForUpdate.score > 380 && playerForUpdate.score <= 500) {
+        document.getElementById('final_score_'+id).style.color = '#ff9933';
+    } else {
+        document.getElementById('final_score_'+id).style.color = '#cc0000';
+    }
+
+    document.getElementById('final_score_'+id).innerText = playerForUpdate.score;
+    
+
+    if (playerForUpdate.score > 500) {
+        document.getElementById('final_score_'+id).innerText = playerForUpdate.score + ' Izgubio/la si!!!! :P';
+    }
+
+    document.getElementById('curent_score_'+id).value = '';
+
+}
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/* LISTA POBEDNIKA */ 
+
+document.getElementById('winners').addEventListener('click', addWinner);
+
+function addWinner(event) {
+    event.preventDefault();
+    const winnersName = document.getElementById('winners-name').value;
+    const date = document.getElementById('date').value;
+
+    save(winnersName, date);
+    showWinners();
+
+    document.getElementById('winners-name').value = '';
+    document.getElementById('date').value = '';
+}
+
+function save(winnersName, date) {
+    let listOfWinners = [];
+    const newWinner = {
+        winnersName: winnersName,
+        date: date
+    }
+
+    if (localStorage.getItem('listOfWinners')) {
+        
+        listOfWinners = JSON.parse(localStorage.getItem('listOfWinners'));
+        
+        if (!listOfWinners.find(winner => winner.winnerName === newWinner.winnerName)) {
+        listOfWinners.push(newWinner);
+        localStorage.setItem('listOfWinners', JSON.stringify(listOfWinners));
+         } 
+    } else {
+        listOfWinners.push(newWinner);
+        localStorage.setItem('listOfWinners', JSON.stringify(listOfWinners));
+    }
+}
+
+function showWinners() {
+    const listOfWinners = JSON.parse(localStorage.getItem('listOfWinners'));
+
+    let winnersTableHtml = "<table class='table table-striped'>";
+    listOfWinners.forEach(winner => {
+        winnersTableHtml += `<tr><td>${winner.winnersName}</td><td>${winner.date}</td></tr>`
+    });
+    winnersTableHtml += "</table>";
+    document.getElementById('content').innerHTML += winnersTableHtml;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// TODO: staviti jedno dugme za unos rezultata
+// staviti delete dugme
+// u input staviti type="number"
+// onesposobiti prazan unos rezultata
